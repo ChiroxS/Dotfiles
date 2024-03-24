76b9b8c6a463c0d44751d8ef1ad5666629275c70
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2024-03-24 19:53:42 +0200

    added swift

--- NvChad2.5/lua/plugins/init.lua
+++ NvChad2.5/lua/plugins/init.lua
@@ -1,39 +1,43 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		config = function()
+			require("configs.conform")
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "gofumpt",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"stylua",
+				"gofumpt",
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "go",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"go",
+				"swift",
+				"rust",
+				"c",
+				"cpp",
+			},
+		},
+	},
 }
