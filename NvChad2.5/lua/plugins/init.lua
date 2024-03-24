8020197eb41ad82dec9307b2f1a10e07faefbdfa
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2024-03-24 14:30:43 +0200

    added 2.5 config

---
+++ NvChad2.5/lua/plugins/init.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "gofumpt",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "go",
+      },
+    },
+  },
+}
