9434da39a92f1b1c0ce2af6441ae06197069f12e
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2024-03-29 14:53:27 +0200

    transparency

--- NvChad2.5/lua/options.lua
+++ NvChad2.5/lua/options.lua
@@ -6,6 +6,8 @@ local o = vim.o
 o.cursorlineopt = "both"
 o.relativenumber = true
 
+require("base46").toggle_transparency()
+
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*.metal" },
 	command = "setfiletype metal",
